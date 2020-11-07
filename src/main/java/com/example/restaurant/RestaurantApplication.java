f384f29b1cbcbcbe17385f025ea22e853fae4dca
Yaser Tirsen <yaser.tirsen@mydit.ie>
2020-11-07 22:09:07 +0000

    added Swagger config for documentation

--- src/main/java/com/example/restaurant/RestaurantApplication.java
+++ src/main/java/com/example/restaurant/RestaurantApplication.java
@@ -1,9 +1,12 @@
 package com.example.restaurant;
 
+import com.example.restaurant.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class RestaurantApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/restaurant/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.example.restaurant.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket restaurantApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Restaurant Ordering API")
+                .version("1.0")
+                .description("API for restaurants to list items and register users with details for delivery and marketing")
+                .contact(new Contact("Yacer Tirsen", "https://www.linkedin.com/in/yaser-tirsen/", "yacer.tirsen@gmail.com"))
+                .license("License")
+                .build();
+    }
+}
